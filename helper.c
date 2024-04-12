2e794313ef808229df2926aed642abebe7c90e30
Francisco Acuña <127992216+franacu@users.noreply.github.com>
2024-04-12 11:32:29 -0300

    inicio

---
+++ helper.c
@@ -0,0 +1,5 @@
+//
+// Created by Fran on 12/04/2024.
+//
+
+#include "helper.h"
---
+++ helper.h
@@ -0,0 +1,8 @@
+//
+// Created by Fran on 12/04/2024.
+//
+#ifndef CLASE4_HELPER_H
+#define CLASE4_HELPER_H
+
+
+#endif //CLASE4_HELPER_H
